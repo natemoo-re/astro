92efb3dac583fbd29e7b38ce1c0227d2e8734318
Nate Moore <nate@skypack.dev>
2021-11-14 17:41:08 -0600

    minify footprint

--- examples/with-nanostores/src/store/admins.js
+++
@@ -1,7 +0,0 @@
-import { createDerived } from 'nanostores';
-
-import { users } from './users.js';
-
-const admins = createDerived(users, (list) => list.filter((user) => user.isAdmin));
-
-export { admins };
