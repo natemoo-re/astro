6442d606a117045266758ee7e5bb051ceb9d9237
Nate Moore <nate@skypack.dev>
2022-05-25 18:03:50 -0500

    chore: update bundle-size action

--- .github/bundle-size.mjs
+++ .github/bundle-size.mjs
@@ -1,4 +1,4 @@
-module.exports = async ({ github, context }) => {
+export default async function checkBundleSize({ github, context }) {
 	console.log({ github, context });
   return context.payload.client_payload.value
 }
