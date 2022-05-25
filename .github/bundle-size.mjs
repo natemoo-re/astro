f5514444054e4d76a0c6908ba5e8da806a91773f
Nate Moore <nate@skypack.dev>
2022-05-25 18:17:57 -0500

    update bundle action

--- .github/bundle-size.mjs
+++ .github/bundle-size.mjs
@@ -1,8 +1,6 @@
 export default async function checkBundleSize({ github, context }) {
-	console.log(context.repo);
 	const files = await github.rest.pulls.listFiles({
-		owner: context.repo.owner,
-		owner: context.repo.repo,
+		...context.repo,
 		pull_number: context.payload.pull_request.number,
 	});
 	console.log({ files });
