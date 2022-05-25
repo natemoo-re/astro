7d11055ea29e8ebef9edc7b6aaa3967d1e62803d
Nate Moore <nate@skypack.dev>
2022-05-25 18:36:17 -0500

    update bundle action

--- .github/bundle-size.mjs
+++ .github/bundle-size.mjs
@@ -1,10 +1,30 @@
-export default async function checkBundleSize({ github, context }) {
+import { build } from 'esbuild';
+
+export default async function checkBundleSize({ github, context, exec }) {
 	const { data: files } = await github.rest.pulls.listFiles({
 		...context.repo,
 		pull_number: context.payload.pull_request.number,
 	});
-	const modifiesClientRuntime = !!files.find(({ filename }) => filename.startsWith('packages/astro/src/runtime/client/'));
-	if (!modifiesClientRuntime) return;
+	const clientRuntimeFiles = files.filter(({ filename }) => filename.startsWith('packages/astro/src/runtime/client/'));
+	if (clientRuntimeFiles.length === 0) return;
 	console.log('Checking bundle size!');
+	const output = await bundle(clientRuntimeFiles);
+	for (const [filename, info] of Object.entries(output)) {
+		console.log(filename, info.bytes);
+	}
 	return
+}
+
+async function bundle(files) {
+	const { metafile } = await build({
+		entryPoints: [files.map(({ filename }) => filename)],
+		bundle: true,
+		minify: true,
+		sourcemap: true,
+		target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
+		outfile: '<stdout>',
+		metafile: true,
+	})
+
+	return metafile.outputs;
 }
