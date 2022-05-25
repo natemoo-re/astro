6946eac5bcef971fbe84ce38cf9e06905e7ec12d
Nate Moore <nate@skypack.dev>
2022-05-25 18:47:03 -0500

    update bundle action

--- .github/bundle-size.mjs
+++ .github/bundle-size.mjs
@@ -1,17 +1,31 @@
 import { build } from 'esbuild';
 
 export default async function checkBundleSize({ github, context, exec }) {
+	const PR_NUM = context.payload.pull_request.number;
 	const { data: files } = await github.rest.pulls.listFiles({
 		...context.repo,
-		pull_number: context.payload.pull_request.number,
+		pull_number: PR_NUM,
 	});
 	const clientRuntimeFiles = files.filter(({ filename }) => filename.startsWith('packages/astro/src/runtime/client/'));
 	if (clientRuntimeFiles.length === 0) return;
-	console.log('Checking bundle size!');
+	
+	const table = [
+		'| File | Size |',
+		'| ---- | ---- |',
+	];
 	const output = await bundle(clientRuntimeFiles);
 	for (const [filename, info] of Object.entries(output)) {
-		console.log(filename, info.bytes);
+		table.push(`| ${filename.slice('.tmp/'.length)} | ${info.bytes} |`);
 	}
+
+	console.log(table.join('\n'));
+
+	const { data: comments } = await github.rest.issues.comments({
+		...context.repo,
+		issue_number: PR_NUM
+	})
+	console.log(comments);
+
 	return
 }
 
@@ -20,7 +34,7 @@ async function bundle(files) {
 		entryPoints: [files.map(({ filename }) => filename)],
 		bundle: true,
 		minify: true,
-		sourcemap: true,
+		sourcemap: false,
 		target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
 		outdir: '.tmp/',
 		metafile: true,
